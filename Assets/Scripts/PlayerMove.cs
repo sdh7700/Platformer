247c427c1a4651463c9012ddf49f11aa272e2a95
donghwa <donghwa811@naver.com>
2021-12-05 11:59:39 +0900

    commit

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,67 +4,67 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-  public float maxSpeed;
-  public float jumpPower;
-  Rigidbody2D rigid;
-  SpriteRenderer spriteRenderer;
-  Animator anim;
+    public float maxSpeed;
+    public float jumpPower;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
 
-  void Awake()
-  {
-    rigid = GetComponent<Rigidbody2D>();
-    spriteRenderer = GetComponent<SpriteRenderer>();
-    anim = GetComponent<Animator>();
-  }
-
-  void Update()
-  {
-    // Jump
-    if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+    void Awake()
     {
-      rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-      anim.SetBool("isJumping", true);
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    // Stop Speed
-    if (Input.GetButtonUp("Horizontal"))
+    void Update()
     {
-      rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        // Jump
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+
+        // Stop Speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        // Direction Sprite
+        if (Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        // Animation
+        if (Mathf.Abs(rigid.velocity.x) < 0.5)
+            anim.SetBool("isWalking", false);
+        else
+            anim.SetBool("isWalking", true);
     }
 
-    // Direction Sprite
-    if (Input.GetButtonDown("Horizontal"))
-      spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
-
-    // Animation
-    if (Mathf.Abs(rigid.velocity.x) < 0.3)
-      anim.SetBool("isWalking", false);
-    else
-      anim.SetBool("isWalking", true);
-  }
-
-  void FixedUpdate()
-  {
-    // Move Speed
-    float h = Input.GetAxisRaw("Horizontal");
-    rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-    // Max Speed
-    if (rigid.velocity.x > maxSpeed) // Left Max Speed
-      rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y); // y축은 현재 속도 그대로(점프할수도 있음)
-    else if (rigid.velocity.x < maxSpeed * (-1)) // Right Max Speed
-      rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
-
-    // Landing Platform
-    if (rigid.velocity.y < 0)
+    void FixedUpdate()
     {
-      Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
-      RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform")); // 단위벡터 1로 거리 적용
-      if (rayHit.collider != null)
-      { // 빔을 쏴서 맞으면, collider가 들어있다(내용물이 있다)
-        if (rayHit.distance < 0.55f)
-          anim.SetBool("isJumping", false);
-      }
+        // Move Speed
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        // Max Speed
+        if (rigid.velocity.x > maxSpeed) // Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y); // y축은 현재 속도 그대로(점프할수도 있음)
+        else if (rigid.velocity.x < maxSpeed * (-1)) // Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        // Landing Platform
+        if (rigid.velocity.y < 0) // 떨어지고 있는 중일 때
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform")); // 단위벡터 1로 거리 적용
+            if (rayHit.collider != null)
+            { // 빔을 쏴서 맞으면, collider가 들어있다(내용물이 있다)
+                if (rayHit.distance < 0.55f)
+                    anim.SetBool("isJumping", false);
+            }
+        }
     }
-  }
 }
